65dc784175744410d0defc675354a4221ac29f89
Gökhan Kocamaz <gk-89-@hotmail.com>
2021-09-16 12:50:45 +0300

    CustomerControllerTest => Added.

--- Tests/CustomerServiceTests/CustomerControllerTests.cs
+++ Tests/CustomerServiceTests/CustomerControllerTests.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using AutoMapper;
 using CustomerService.Controllers;
-using CustomerService.Filters;
 using CustomerService.Utility;
 using Entities.Models;
 using Entities.RequestModels;
-using FluentAssertions;
+using Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Repository;
@@ -18,7 +19,7 @@ namespace CustomerServiceTests
 {
     public class CustomerControllerTests
     {
-        
+
         private readonly Mock<IRepository<Customer>> _mockRepository = new();
         private readonly IMapper _mapper;
 
@@ -26,16 +27,13 @@ namespace CustomerServiceTests
         {
             if (_mapper == null)
             {
-                var mappingConfig = new MapperConfiguration(mc =>
-                {
-                    mc.AddProfile(new MappingProfile());
-                });
+                var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
                 IMapper mapper = mappingConfig.CreateMapper();
                 _mapper = mapper;
             }
         }
-        
-        
+
+        // Get Tests
         [Fact]
         public async Task GetCustomer_ExistingId_ReturnsOkObject()
         {
@@ -43,14 +41,119 @@ namespace CustomerServiceTests
                 .ReturnsAsync(new Customer());
             var customersController = new CustomersController(_mockRepository.Object, _mapper);
             var result = await customersController.GetCustomer(Guid.NewGuid());
-            
+
             Assert.IsType<OkObjectResult>(result);
         }
 
-        // [Fact]
-        // public async Task CreateCustomer_RegisteredEmail_ReturnsConflict()
-        // {
-        //     _mockRepository.Setup(r => r.CreateAsync(It.IsAny<Customer>()));
-        // }
+        [Fact]
+        public async Task GetAllCustomers_ReturnsOkObject()
+        {
+            _mockRepository.Setup(r => r.GetAll())
+                .ReturnsAsync(new List<Customer>());
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            var result = await controller.GetAllCustomers();
+
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+
+
+        // Create Tests
+
+        [Fact]
+        public async Task CreateCustomer_NotUniqueId_ReturnsConflict()
+        {
+            var returns = Mock.Of<List<Customer>>();
+            returns.Add(new Customer());
+            // Mock.Of, List customer'ı null olarak üretiyor. Bu sebeple controller exception üretemiyor.
+
+            _mockRepository.Setup(x => x.GetByCondition(It.IsAny<Expression<Func<Customer, bool>>>()))
+                .ReturnsAsync(returns);
+            _mockRepository.Setup(x => x.CreateAsync(It.IsAny<Customer>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            await Assert.ThrowsAsync<EmailIsNotUniqueException>(() =>
+                controller.CreateCustomer(Mock.Of<CreateCustomerDto>()));
+        }
+
+        [Fact]
+        public async Task CreateCustomer_UniqueEmail_ReturnsCreatedAtRoute()
+        {
+            _mockRepository.Setup(x => x.GetByCondition(It.IsAny<Expression<Func<Customer, bool>>>()))
+                .ReturnsAsync(new List<Customer>());
+            _mockRepository.Setup(x => x.CreateAsync(It.IsAny<Customer>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            
+            var result = await controller.CreateCustomer(Mock.Of<CreateCustomerDto>());
+
+            Assert.IsType<CreatedAtRouteResult>(result);
+
+        }
+        
+
+        /*[Fact]
+        public async Task CreateCustomer_ModelNullProperty_ReturnsError()
+        {
+            _mockRepository.Setup(x => x.GetByCondition(It.IsAny<Expression<Func<Customer, bool>>>()))
+                .ReturnsAsync(new List<Customer>());
+            _mockRepository.Setup(x => x.CreateAsync(It.IsAny<Customer>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            await Assert.ThrowsAsync<EmailIsNotUniqueException>(() =>
+                controller.CreateCustomer(Mock.Of<CreateCustomerDto>()));
+            
+        }*/
+        
+        [Fact]
+        public async Task UpdateCustomer_NotUniqueId_ReturnsConflict()
+        {
+            var returns = Mock.Of<List<Customer>>();
+            returns.Add(new Customer());
+            // Mock.Of, List customer'ı null olarak üretiyor. Bu sebeple controller exception üretemiyor.
+
+            _mockRepository.Setup(x => x.GetByCondition(It.IsAny<Expression<Func<Customer, bool>>>()))
+                .ReturnsAsync(returns);
+            _mockRepository.Setup(x => x.ReplaceAsync(It.IsAny<Customer>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            _mockRepository.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            await Assert.ThrowsAsync<EmailIsNotUniqueException>(() =>
+                controller.UpdateCustomer(It.IsAny<Guid>(), Mock.Of<UpdateCustomerDto>()));
+        }
+        
+        
+        
+        [Fact]
+        public async Task UpdateCustomer_UniqueEmail_ReturnsOkObject()
+        {
+            _mockRepository.Setup(x => x.GetByCondition(It.IsAny<Expression<Func<Customer, bool>>>()))
+                .ReturnsAsync(new List<Customer>());
+            _mockRepository.Setup(x => x.CreateAsync(It.IsAny<Customer>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            _mockRepository.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(It.IsAny<Customer>);
+            var controller = new CustomersController(_mockRepository.Object, _mapper);
+            
+            var result = await controller.UpdateCustomer(It.IsAny<Guid>(),Mock.Of<UpdateCustomerDto>());
+
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+
+        [Fact]
+        public async Task DeleteCustomer_ExistingCustomerId_ReturnsOkObject()
+        {
+            _mockRepository.Setup(r => r.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new Customer());
+            _mockRepository.Setup(r => r.Delete(It.IsAny<Customer>()));
+            var customersController = new CustomersController(_mockRepository.Object, _mapper);
+            var result = await customersController.DeleteCustomer(Guid.NewGuid());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
     }
 }
